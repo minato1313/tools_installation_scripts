2860ed6ce82eaeb0663c23d7d6df9baa527a8e16
Vishal Morchhle <143252661+minato1313@users.noreply.github.com>
2024-12-20 00:40:17 +0530

    installing kubectl & eksctl

---
+++ kubectl_eksctl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+# Install required dependencies
+sudo apt-get install -y curl unzip python3 python3-pip gnupg software-properties-common
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+# Verify installations
+aws --version && kubectl version --client && eksctl version

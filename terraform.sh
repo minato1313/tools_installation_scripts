518055dd7120e262a245e250153c13540f01444f
Vishal Morchhle <143252661+minato1313@users.noreply.github.com>
2024-12-20 00:48:41 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -2,24 +2,20 @@
 
 sudo apt-get update -y
 
-# Install required dependencies for AWS CLI and Terraform
+# Install required dependencies
 sudo apt-get install -y curl unzip python3 python3-pip gnupg software-properties-common
 
 # Install AWS CLI
-if ! command -v aws &> /dev/null; then
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  sudo ./aws/install
-  rm -rf awscliv2.zip aws
-fi
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
 
 # Install Terraform
-if ! command -v terraform &> /dev/null; then
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-  sudo apt-get update -y
-  sudo apt-get install -y terraform
-fi
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update -y
+sudo apt-get install -y terraform
 
 # Verify installations
 aws --version && terraform --version
